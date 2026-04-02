76479c713a947913d23d1536c6193776a281ae19
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-04-02 21:23:12 +0300

    Add user class
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ proiect_isp/src/Utilizator.java
@@ -0,0 +1,68 @@
+public class Utilizator {
+    // Atribute
+    private int id;
+    private String email, numeUtilizator, parola, rol;
+
+    // Getters, Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumeUtilizator() {
+        return numeUtilizator;
+    }
+
+    public void setNumeUtilizator(String numeUtilizator) {
+        this.numeUtilizator = numeUtilizator;
+    }
+
+    public String getParola() {
+        return parola;
+    }
+
+    public void setParola(String parola) {
+        this.parola = parola;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    // Constructor default
+    public Utilizator() {}
+
+    // Constructor cu parametri
+    public Utilizator(int id, String email, String numeUtilizator, String parola, String rol) {
+       this.id = id;
+       this.email = email;
+       this.numeUtilizator = numeUtilizator;
+       this.parola = parola;
+       this.rol = rol;
+    }
+
+    public Utilizator(String email, String numeUtilizator, String parola, String rol) {
+        this.email = email;
+        this.numeUtilizator = numeUtilizator;
+        this.parola = parola;
+        this.rol = rol;
+    }
+
+    // Metode
+    // Autentificare
+}
