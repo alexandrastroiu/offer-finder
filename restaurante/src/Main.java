1dbd30852457ff2508b0b8e1fd6780b1a984ca34
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-05-06 11:17:48 +0300

    Add login
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- restaurante/src/Main.java
+++ restaurante/src/Main.java
@@ -1,10 +1,37 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        Utilizator utilizator1 = new Utilizator();
+        Utilizator utilizator2 = new Utilizator();
+        Utilizator utilizator3 = new Utilizator();
+
+        System.out.println("---------------------------------------------------------------");
+        System.out.println("                 Comparator Oferte Restaurante                 ");
+        System.out.println("---------------------------------------------------------------");
+        System.out.println("\nIntroduceti datele pentru autentificare: ");
+        System.out.print("Nume utilizator: ");
+        String numeUtilizator = scanner.nextLine();
+        System.out.print("Parola: ");
+        String parola = scanner.nextLine();
+
+        // Login
+        boolean esteAutentificat = utilizator1.autentificare(numeUtilizator, parola, "../data/utilizatori.csv");
+
+        if (esteAutentificat) {
+            System.out.println("\nAutentificare reusita!");
+        }
+        else {
+            System.out.println("\nAutentificare esuata!");
+            System.out.println("Nume utilizator sau parola incorecta.");
+        }
+
+        scanner.close();
     }
 
 }
